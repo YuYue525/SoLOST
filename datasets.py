b0b086a115e723946f70e4321e0ae4ca83b76cd9
YuYue525 <yuyue1999525@gmail.com>
2022-11-12 20:25:06 +0800

    datasets.py init

--- datasets.py
+++ datasets.py
@@ -1,5 +1,6 @@
 import os
 import math
+import json
 import torchvision
 import skimage.io
 
@@ -28,7 +29,17 @@ class Dataset:
 
             self.dataloader = torchvision.datasets.VOCDetection(self.root_path, year=self.year, image_set=self.set,
                                                                 transform=transform, download=True)
-        # elif self.dataset_name == "COCO2k":
+        elif self.dataset_name == "COCO2k":
+            self.year = "2014"
+            self.root_path = f"datasets/COCO/images/{self.set}{self.year}"
+
+            self.sel20k = 'datasets/coco_20k_filenames.txt'
+            self.all_annfile = "datasets/COCO/annotations/instances_train2014.json"
+
+            self.annfile = "datasets/instances_train2014_sel20k.json"
+
+            if not os.path.exists(self.annfile):
+                select_20k_for_coco(self.sel20k, self.all_annfile)
 
         else:
             raise ValueError("Unknown dataset.")
@@ -54,6 +65,39 @@ class Dataset:
             img_name = inp["annotation"]["filename"]
 
         return img_name
+
+def select_20k_for_coco(sel_file, all_annFile):
+    print('Building COCO 20k dataset.')
+
+    # all the annotations
+    with open(all_annFile, "r") as f:
+        train2014 = json.load(f)
+
+    # selected images
+    with open(sel_file, "r") as f:
+        sel_20k = f.readlines()
+        sel_20k = [s.replace("\n", "") for s in sel_20k]
+
+    im20k = [str(int(s.split("_")[-1].split(".")[0])) for s in sel_20k]
+
+    new_anno = []
+    new_images = []
+
+    for i in tqdm(im20k):
+        new_anno.extend(
+            [a for a in train2014["annotations"] if a["image_id"] == int(i)]
+        )
+        new_images.extend([a for a in train2014["images"] if a["id"] == int(i)])
+
+    train2014_20k = {}
+    train2014_20k["images"] = new_images
+    train2014_20k["annotations"] = new_anno
+    train2014_20k["categories"] = train2014["categories"]
+
+    with open("datasets/instances_train2014_sel20k.json", "w") as outfile:
+        json.dump(train2014_20k, outfile)
+
+    print('Done.')
 
 '''
 if __name__ == '__main__':

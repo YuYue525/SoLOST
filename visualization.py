66170345c21e9b347f65d4c904bcff68230dffc4
YuYue525 <yuyue1999525@gmail.com>
2022-11-13 13:04:47 +0800

    init

--- visualization.py
+++ visualization.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 import matplotlib.pyplot as plt
 
-def visualize_predictions(image, pred, seed, scales, dims, vis_folder, im_name, plot_seed=False):
+def visualize_predictions(image, pred, seed, scales, dims, vis_folder, img_name, plot_seed=False):
     w_featmap, h_featmap = dims
 
     cv2.rectangle(image,
@@ -26,38 +26,34 @@ def visualize_predictions(image, pred, seed, scales, dims, vis_folder, im_name, 
             (int(s_[1] * scales[1] + (size_[1] / 2)), int(s_[0] * scales[0] + (size_[0] / 2))),
             (0, 255, 0), -1)
 
-    pltname = f"{vis_folder}/LOST_{im_name}.png"
-    Image.fromarray(image).save(pltname)
-    print(f"Predictions saved at {pltname}.")
+    result_path = f"{vis_folder}/LOST_{img_name}.png"
+    Image.fromarray(image).save(result_path)
+    print(f"Predictions saved at {result_path}.")
 
-def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
-
+def visualize_fms(A, seed, scores, dims, scales, output_folder, img_name):
     w_featmap, h_featmap = dims
 
     binA = A.copy()
     binA[binA < 0] = 0
     binA[binA > 0] = 1
 
-    im_corr = np.zeros((3, len(scores)))
+    img_corr = np.zeros((3, len(scores)))
     where = binA[seed, :] > 0
-    im_corr[:, where] = np.array([128 / 255, 133 / 255, 133 / 255]).reshape((3, 1))
+    img_corr[:, where] = np.array([128 / 255, 133 / 255, 133 / 255]).reshape((3, 1))
 
-    im_corr[:, seed] = [204 / 255, 37 / 255, 41 / 255]
+    img_corr[:, seed] = [204 / 255, 37 / 255, 41 / 255]
 
-    im_corr = im_corr.reshape((3, w_featmap, h_featmap))
-    im_corr = (
+    img_corr = img_corr.reshape((3, w_featmap, h_featmap))
+    img_corr = (
         nn.functional.interpolate(
-            torch.from_numpy(im_corr).unsqueeze(0),
+            torch.from_numpy(img_corr).unsqueeze(0),
             scale_factor=scales,
             mode="nearest",
         )[0].cpu().numpy()
     )
 
-    skimage.io.imsave(
-        fname=f"{output_folder}/corr_{im_name}.png",
-        arr=im_corr.transpose((1, 2, 0)),
-    )
-    print(f"Image saved at {output_folder}/corr_{im_name}.png .")
+    skimage.io.imsave(fname=f"{output_folder}/corr_{img_name}.png", arr=img_corr.transpose((1, 2, 0)))
+    print(f"Image saved at {output_folder}/corr_{img_name}.png .")
 
     im_deg = (
         nn.functional.interpolate(
@@ -66,10 +62,10 @@ def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
             mode="nearest",
         )[0][0].cpu().numpy()
     )
-    plt.imsave(fname=f"{output_folder}/deg_{im_name}.png", arr=im_deg)
-    print(f"Image saved at {output_folder}/deg_{im_name}.png .")
+    plt.imsave(fname=f"{output_folder}/deg_{img_name}.png", arr=im_deg)
+    print(f"Image saved at {output_folder}/deg_{img_name}.png .")
 
-def visualize_seed_expansion(image, pred, seed, pred_seed, scales, dims, vis_folder, im_name):
+def visualize_seed_expansion(image, pred, seed, pred_seed, scales, dims, vis_folder, img_name):
     w_featmap, h_featmap = dims
 
     cv2.rectangle(image,
@@ -91,6 +87,6 @@ def visualize_seed_expansion(image, pred, seed, pred_seed, scales, dims, vis_fol
     image[start_1:end_1, start_2:end_2, 1] = 37
     image[start_1:end_1, start_2:end_2, 2] = 41
 
-    pltname = f"{vis_folder}/LOST_seed_expansion_{im_name}.png"
-    Image.fromarray(image).save(pltname)
-    print(f"Image saved at {pltname}.")
+    result_path = f"{vis_folder}/LOST_seed_expansion_{img_name}.png"
+    Image.fromarray(image).save(result_path)
+    print(f"Image saved at {result_path}.")

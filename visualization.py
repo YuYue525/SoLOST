4e5ea8ad67afa6ccc99d7df1df94668a20094f5c
YuYue525 <yuyue1999525@gmail.com>
2022-11-13 12:58:01 +0800

    init

--- visualization.py
+++ visualization.py
@@ -11,52 +11,39 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 def visualize_predictions(image, pred, seed, scales, dims, vis_folder, im_name, plot_seed=False):
-    """
-    Visualization of the predicted box and the corresponding seed patch.
-    """
     w_featmap, h_featmap = dims
 
-    # Plot the box
-    cv2.rectangle(
-        image,
+    cv2.rectangle(image,
         (int(pred[0]), int(pred[1])),
         (int(pred[2]), int(pred[3])),
-        (255, 0, 0), 3,
-    )
+        (255, 0, 0), 3)
 
-    # Plot the seed
     if plot_seed:
         s_ = np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))
         size_ = np.asarray(scales) / 2
-        cv2.rectangle(
-            image,
+        cv2.rectangle(image,
             (int(s_[1] * scales[1] - (size_[1] / 2)), int(s_[0] * scales[0] - (size_[0] / 2))),
             (int(s_[1] * scales[1] + (size_[1] / 2)), int(s_[0] * scales[0] + (size_[0] / 2))),
-            (0, 255, 0), -1,
-        )
+            (0, 255, 0), -1)
 
     pltname = f"{vis_folder}/LOST_{im_name}.png"
     Image.fromarray(image).save(pltname)
     print(f"Predictions saved at {pltname}.")
 
 def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
-    """
-    Visualization of the maps presented in Figure 2 of the paper.
-    """
+
     w_featmap, h_featmap = dims
 
-    # Binarized similarity
     binA = A.copy()
     binA[binA < 0] = 0
     binA[binA > 0] = 1
 
-    # Get binarized correlation for this pixel and make it appear in gray
     im_corr = np.zeros((3, len(scores)))
     where = binA[seed, :] > 0
     im_corr[:, where] = np.array([128 / 255, 133 / 255, 133 / 255]).reshape((3, 1))
-    # Show selected pixel in green
+
     im_corr[:, seed] = [204 / 255, 37 / 255, 41 / 255]
-    # Reshape and rescale
+
     im_corr = im_corr.reshape((3, w_featmap, h_featmap))
     im_corr = (
         nn.functional.interpolate(
@@ -66,14 +53,12 @@ def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
         )[0].cpu().numpy()
     )
 
-    # Save correlations
     skimage.io.imsave(
         fname=f"{output_folder}/corr_{im_name}.png",
         arr=im_corr.transpose((1, 2, 0)),
     )
     print(f"Image saved at {output_folder}/corr_{im_name}.png .")
 
-    # Save inverse degree
     im_deg = (
         nn.functional.interpolate(
             torch.from_numpy(1 / binA.sum(-1)).reshape(1, 1, w_featmap, h_featmap),
@@ -85,30 +70,18 @@ def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
     print(f"Image saved at {output_folder}/deg_{im_name}.png .")
 
 def visualize_seed_expansion(image, pred, seed, pred_seed, scales, dims, vis_folder, im_name):
-    """
-    Visualization of the seed expansion presented in Figure 3 of the paper.
-    """
     w_featmap, h_featmap = dims
 
-    # Before expansion
-    cv2.rectangle(
-        image,
+    cv2.rectangle(image,
         (int(pred_seed[0]), int(pred_seed[1])),
         (int(pred_seed[2]), int(pred_seed[3])),
-        (204, 204, 0),  # Yellow
-        3,
-    )
+        (204, 204, 0), 3)
 
-    # After expansion
-    cv2.rectangle(
-        image,
+    cv2.rectangle(image,
         (int(pred[0]), int(pred[1])),
         (int(pred[2]), int(pred[3])),
-        (204, 0, 204),  # Magenta
-        3,
-    )
+        (204, 0, 204), 3)
 
-    # Position of the seed
     center = np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))
     start_1 = center[0] * scales[0]
     end_1 = center[0] * scales[0] + scales[0]
